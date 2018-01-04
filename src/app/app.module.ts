18d0ead0825ff37ea07c435713d91ad2adfd2a23
chicheongweng <chicheongweng@gmail.com>
2018-01-04 14:03:00 +0800

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SecuritiesComponent } from './securities/securities.component';
+import { FuturesComponent } from './futures/futures.component';
+import { LoansComponent } from './loans/loans.component';
+import { CashComponent } from './cash/cash.component';
+import { BankFacilitiesComponent } from './bank-facilities/bank-facilities.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecuritiesComponent,
+    FuturesComponent,
+    LoansComponent,
+    CashComponent,
+    BankFacilitiesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/bank-facilities/bank-facilities.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bank-facilities',
+  templateUrl: './bank-facilities.component.html',
+  styleUrls: ['./bank-facilities.component.css']
+})
+export class BankFacilitiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

14629b635eb77cf529aaa0bd9d89f511ff77ac9b
chicheongweng <chicheongweng@gmail.com>
2018-01-05 09:44:03 +0800

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SecuritiesComponent } from './securities.component';
+import { FuturesComponent } from './futures.component';
+import { LoansComponent } from './loans.component';
+import { CashComponent } from './cash.component';
+import { BankFacilitiesComponent } from './bank-facilities.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecuritiesComponent,
+    FuturesComponent,
+    LoansComponent,
+    CashComponent,
+    BankFacilitiesComponent
   ],
   imports: [
     BrowserModule

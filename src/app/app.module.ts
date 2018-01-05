abb81531b95b7c63cf71f7c8826a171905dad250
chicheongweng <chicheongweng@gmail.com>
2018-01-05 09:28:28 +0800

    Revert "added components"
    This reverts commit 18d0ead0825ff37ea07c435713d91ad2adfd2a23.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { SecuritiesComponent } from './securities/securities.component';
-import { FuturesComponent } from './futures/futures.component';
-import { LoansComponent } from './loans/loans.component';
-import { CashComponent } from './cash/cash.component';
-import { BankFacilitiesComponent } from './bank-facilities/bank-facilities.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SecuritiesComponent,
-    FuturesComponent,
-    LoansComponent,
-    CashComponent,
-    BankFacilitiesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule

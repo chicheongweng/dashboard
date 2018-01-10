23bdc03104dd2ebd809ff6075f5ba6e7aee3b191
chicheongweng <chicheongweng@gmail.com>
2018-01-10 18:15:41 +0800

    randomized mock data, added href links in main page

--- src/app/mock-data.ts
+++ src/app/mock-data.ts
@@ -5,51 +5,60 @@ import { Loan } from './loans/loan';
 import { Security } from './securities/security';
 import { BankFacility } from './bank-facilities/bank-facility';
 
+function getRandomFloat(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(getRandomFloat(min, max));
+}
+
 export const FUTURES: Future[] = [
   {
     account_executive: '1234',
-    daily_turnover: 100000,
-    accumulated_turnover: 8000000
+    daily_turnover: getRandomInt(5000, 5000000),
+    accumulated_turnover: getRandomInt(5000, 5000000)
   },
   {
     account_executive: '5678',
-    daily_turnover: 990000,
-    accumulated_turnover: 89730000
+    daily_turnover: getRandomInt(5000, 5000000),
+    accumulated_turnover: getRandomInt(5000, 5000000)
   }
 ];
 
 export const SECURITIES: Security[] = [
   {
     account_executive: '1234',
-    daily_turnover: 100000,
-    accumulated_turnover: 8000000
+    daily_turnover: getRandomInt(5000, 5000000),
+    accumulated_turnover: getRandomInt(5000, 5000000)
   },
   {
     account_executive: '5678',
-    daily_turnover: 103900,
-    accumulated_turnover: 89720000
+    daily_turnover: getRandomInt(5000, 5000000),
+    accumulated_turnover: getRandomInt(5000, 5000000),
   }
 ];
 
 export const CASH: Cash[] = [
   {
-    cash: 999999
+    cash: getRandomInt(5000, 5000000)
   }
 ];
+
 export const LOANS: Loan[] = [
   {
-    term: 1000000,
-    mortgage_house: 2000000,
-    mortgage_marketing: 3000000,
-    personal: 4000000,
-    margin: 5000000,
-    advanced: 6000000
+    term: getRandomInt(5000, 5000000),
+    mortgage_house: getRandomInt(5000, 5000000),
+    mortgage_marketing: getRandomInt(5000, 5000000),
+    personal: getRandomInt(5000, 5000000),
+    margin: getRandomInt(5000, 5000000),
+    advanced: getRandomInt(5000, 5000000)
   }
 ];
 
 export const BANK_FACILITIES: BankFacility[] = [
   {
-    utilized: 1000000,
-    unutilized: 2000000
+    utilized: getRandomInt(5000, 5000000),
+    unutilized: getRandomInt(5000, 5000000)
   }
 ];

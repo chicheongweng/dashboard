a83062a1859221e21b9e56ad825e1f49dde4f37c
chicheongweng <chicheongweng@gmail.com>
2018-01-18 15:40:45 +0800

    fixed title in securities.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { SecuritiesComponent } from './securities/securities.component';
 import { FuturesComponent } from './futures/futures.component';

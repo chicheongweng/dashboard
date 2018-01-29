4a33dfca8e6fa4a1b039d6b26e5b4d0e716a0d51
chicheongweng <chicheongweng@gmail.com>
2018-01-29 16:36:50 +0800

    added flashing code

--- src/app/amount/amount.component.ts
+++ src/app/amount/amount.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+
+// reference
+// 1. https://www.themarketingtechnologist.co/building-nested-components-in-angular-2/
+// 2. https://medium.com/@christophkrautz/testing-ngonchanges-in-angular-components-bbb3b4650ee8
 
 @Component({
   selector: 'app-amount',
   templateUrl: './amount.component.html',
   styleUrls: ['./amount.component.css']
 })
-export class AmountComponent implements OnInit {
+export class AmountComponent implements OnChanges {
   @Input() amount: number;
+  cssClass;
 
   constructor() { }
 
-  ngOnInit() {
+  getCSSClass() {
+    this.cssClass = {
+      'background-color': 'white',
+      '-webkit-animation-name': 'example', /* Safari 4.0 - 8.0 */
+      '-webkit-animation-duration': '1s', /* Safari 4.0 - 8.0 */
+      'animation-name': 'example',
+      'animation-duration': '1s'
+    };
   }
 
+  ngOnChanges() {
+    console.log('changes detected');
+    this.getCSSClass();
+  }
 }

f4a06ea127aacbf0620bffd2931c926286977538
chicheongweng <chicheongweng@gmail.com>
2018-01-30 12:48:38 +0800

    make future flash

--- src/app/futures/futures.component.ts
+++ src/app/futures/futures.component.ts
@@ -13,16 +13,28 @@ export class FuturesComponent implements OnInit {
   apiUrl;
   params;
 
+  constructor(private dataService: DataService) {
+    this.apiUrl = environment.apiUrl;
+    this.params = environment.params;
+  }
+
   ngOnInit() {
-    this.reloadItems(this.params);
+    this.dataService.getFuture(this.params).subscribe(
+      resp => {
+        this.currentTime = new Date();
+        this.items = resp.body;
+        this.itemCount = Number(resp.headers.get('X-Total-Count'));
+      }
+    );
     setInterval(() => {
       this.reloadItems(this.params);
     }, environment.poll_freq);
   }
 
-  constructor(private dataService: DataService) {
-    this.apiUrl = environment.apiUrl;
-    this.params = environment.params;
+  private copy(from, to) {
+    to.account_executive = from.account_executive;
+    to.daily_turnover = from.daily_turnover;
+    to.accumulated_turnover = from.accumulated_turnover;
   }
 
   reloadItems(params) {
@@ -30,7 +42,10 @@ export class FuturesComponent implements OnInit {
       resp => {
         this.params = params;
         this.currentTime = new Date();
-        this.items = resp.body;
+        // this.items = resp.body;
+        for (let _i = 0; _i < resp.body.length; _i++) {
+          this.copy(resp.body[_i], this.items[_i]);
+        }
         this.itemCount = Number(resp.headers.get('X-Total-Count'));
       }
     );
